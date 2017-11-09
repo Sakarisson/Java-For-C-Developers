e208518878ca75fd4c967a60eddba210290643f2
Kristian Sakarisson <kristian@sakarisson.com>
2017-11-09 11:45:34 +0000

    Added basic exception class

---
+++ src/dt062g/krsa1201/assignment3/ShapeException.java
@@ -0,0 +1,20 @@
+package dt062g.krsa1201.assignment3;
+
+/**
+ * <h1>Shape Exception</h1>
+ * <p>A simple exception class used for handling
+ * various shape-related errors</p>
+ * @author Kristian Sakarisson
+ * @version 1.0
+ * @since 09-11-2017
+ */
+class ShapeException extends Exception {
+    /**
+     * <h2>Message constructor</h2>
+     * <p>Constructs a new ShapeException with a given message</p>
+     * @param message The details of the error message. Can be accessed with the getMessage() method
+     */
+    public ShapeException(String message) {
+        super(message);
+    }
+}
