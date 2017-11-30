02cd5f9b114796745d361bf1bef51b76217f6350
Kristian Sakarisson <kristian@sakarisson.com>
2017-11-30 14:50:29 +0000

    Added menus

--- src/dt062g/krsa1201/assignment6/Interface.java
+++ src/dt062g/krsa1201/assignment6/Interface.java
@@ -18,6 +18,13 @@ class Interface {
 
     // Private variables
     private JFrame _frame;
+    private Color[] _colors = { 
+        Color.green, 
+        Color.blue, 
+        Color.black, 
+        Color.red, 
+        Color.yellow 
+    };
 
     /**
      * <h1>Default constructor</h1>
@@ -38,12 +45,69 @@ class Interface {
     }
 
     /**
+     * <h2>Create Menu</h2>
+     * <p>Creates the top menu bar of the program</h2>
+     */
+    private void CreateMenu() {
+        // Create Menu
+        JMenuBar menuBar = new JMenuBar();
+
+        // Create File menu option
+        JMenu file = new JMenu("File");
+
+        JMenuItem fileNew = new JMenuItem("New...");
+        file.add(fileNew);
+
+        JMenuItem fileSaveAs = new JMenuItem("Save As...");
+        file.add(fileSaveAs);
+
+        JMenuItem fileLoad = new JMenuItem("Load...");
+        file.add(fileLoad);
+
+        file.addSeparator();
+
+        JMenuItem fileExit = new JMenuItem("Exit");
+        file.add(fileExit);
+
+        // Create Edit menu option
+        JMenu edit = new JMenu("Edit");
+
+        JMenuItem editUndo = new JMenuItem("Undo");
+        edit.add(editUndo);
+
+        JMenuItem editName = new JMenuItem("Name...");
+        edit.add(editName);
+
+        JMenuItem editAuthor = new JMenuItem("Author...");
+        edit.add(editAuthor);
+
+        // Add menus
+        menuBar.add(file);
+        menuBar.add(edit);
+
+        // Set menu to main frame
+        this._frame.setJMenuBar(menuBar);
+    }
+
+    /**
      * <h2>Initialize</h2>
      * <p>Sets up the window to become a proper GUI</p>
      */
     private void Initialize() {
         this._frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this._frame.getContentPane().add(new JLabel("Kristian"), BorderLayout.CENTER);
+        CreateMenu();
+        JPanel shapePickGrid = new JPanel(new GridLayout());
+        shapePickGrid.setPreferredSize(new Dimension(this._frame.getSize().width, 40));
+
+        for (Color c: this._colors) {
+            JPanel colorOption = new JPanel();
+            colorOption.setBackground(c);
+            shapePickGrid.add(colorOption);
+        }
+        String[] shapes = { "Rectangle", "Circle" };
+        JComboBox<String> shapePicker = new JComboBox<String>(shapes);
+        shapePickGrid.add(shapePicker);
+        this._frame.add(shapePickGrid, BorderLayout.PAGE_START);
         this._frame.pack();
         this._frame.setVisible(true);
     }
